bcde8d4d8765420478a5259f5f8df43589a106a2
Mahantesh Patil <patilmahantesh642@gmail.com>
2026-08-21 11:36:44 +0530

    Time: 42 ms (5.03%) | Memory: 46.1 MB (93.43%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int calculate(String s) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int result = 0;
+        int sign = 1; // 1 represents '+', -1 represents '-'
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                int num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                i--; // Step back since the for-loop increments i
+                result += sign * num;
+            } else if (c == '+') {
+                sign = 1;
+            } else if (c == '-') {
+                sign = -1;
+            } else if (c == '(') {
+                // Push current scope state
+                stack.push(result);
+                stack.push(sign);
+                // Reset for the inner scope
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                // Pop inner scope sign and previous result
+                int prevSign = stack.pop();
+                int prevResult = stack.pop();
+                result = prevResult + prevSign * result;
+            }
+        }
+
+        return result;
+    }
+}

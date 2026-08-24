1db9fd3f890546856b4bec14e7b2557b0e5ae7b5
Mahantesh Patil <patilmahantesh642@gmail.com>
2026-08-24 21:27:07 +0530

    Time: 1 ms (99.98%) | Memory: 43.7 MB (45.20%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,17 +1,18 @@
 class Solution {
     public String makeGood(String s) {
         StringBuilder sb = new StringBuilder();
-        for(char c : s.toCharArray()){
+
+        for (char c : s.toCharArray()) {
             int len = sb.length();
 
-            if(len > 0 && Character.toLowerCase(sb.charAt(len-1)) == Character.toLowerCase(c) && sb.charAt(len-1) != c) {
-                sb.deleteCharAt(len-1);
-            }
-            else{
-                sb.append(c);
+            // Check if the current character forms a bad pair with the last character in sb
+            if (len > 0 && Math.abs(sb.charAt(len - 1) - c) == 32) {
+                sb.deleteCharAt(len - 1); // Pop the adjacent character
+            } else {
+                sb.append(c);             // Push the character
             }
         }
+
         return sb.toString();
-        
     }
 }

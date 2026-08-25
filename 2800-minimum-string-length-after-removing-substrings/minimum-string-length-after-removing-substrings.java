0fde27f657162f9ba71604dcf10255010782cfbe
Mahantesh Patil <patilmahantesh642@gmail.com>
2026-08-25 11:14:44 +0530

    Time: 3 ms (95.89%) | Memory: 44.3 MB (95.10%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minLength(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            int len = sb.length();
+            if(len > 0){
+            char last = sb.charAt(len-1);
+              if((last == 'A' && c == 'B') || (last == 'C' && c == 'D')){
+                sb.deleteCharAt(len-1);
+                continue;
+            }
+            }
+                sb.append(c);
+        }
+        return sb.length();
+    }
+}
